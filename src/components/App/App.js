ac246c53e78c01862cedd43a26fb563dfd160eee
Anne Culpepper <anne.culpepper@gmail.com>
2020-04-17 16:34:39 -0500

    semantic markup for app.js. Created 4 Views files within components/views folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+// import Feeling from "../Views/Feeling";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
+        <Router>
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4>
+              <i>Don't forget it!</i>
+            </h4>
+          </header>
+          <body>
+            <h1>How are you feeling today?</h1>
+            <div>
+              <form>
+                <input type="text" placeholder="How are you feeling?" />
+                <button onClick={this.onNextClick}>Next</button>
+              </form>
+            </div>
+          </body>
+          <br />
+        </Router>
       </div>
     );
   }
---
+++ src/components/Views/Feeling.js
@@ -0,0 +1,30 @@
+//might not need this file -- starting feeling question from app.js
+
+// import React, { Component } from "react";
+// import { connect } from "react-redux";
+
+// class Feeling extends Component {
+//   state = {
+//     feeling: 0,
+//   };
+
+//   render() {
+//     console.log(this.props.store);
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <h1 className="App-title">Feedback!</h1>
+//           <h4>
+//             <i>Don't forget it!</i>
+//           </h4>
+//         </header>
+//         <br />
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStoreToProps = (store) => ({ store });
+
+// export default connect(mapStoreToProps)(Feeling);
